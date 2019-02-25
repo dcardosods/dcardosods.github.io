8fd5e422d65d30a2dd6fd9b375b5528112e10920
Daniel Cardoso <danielcardosods@gmail.com>
2019-02-25 14:59:03 -0300

    Add custom icon/logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,15 +32,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Daniel Cardoso dS',
+        short_name: 'dcardosods',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: 'grey',
+        theme_color: 'grey',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/dcds-icon.svg', // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-react-svg',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,31 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
+import typography from './../utils/typography'
+import Logo from './../images/dcds-icon.svg'
+
 const Header = ({ siteTitle }) => (
   <header style={{ borderBottom: '1px solid grey' }}>
-    <Link to="/" style={{ background: 'none', textShadow: 'none' }}>
+    <Link
+      to="/"
+      style={{
+        background: 'none',
+        textShadow: 'none',
+        color: typography.options.headerColor,
+      }}
+    >
+      <Logo style={{ width: '48px', height: '48px', verticalAlign: 'super' }} />
       <h1
         style={{
           padding: '8px',
           display: 'inline-block',
-          fontSize: '44px',
+          fontSize: '37px',
           marginBottom: '1px',
         }}
       >
         <div>Daniel Cardoso</div>
         <div
-          style={{ textAlign: 'right', fontSize: '20px', marginTop: '-8px' }}
+          style={{ textAlign: 'right', fontSize: '28px', marginTop: '-8px' }}
         >
           dS
         </div>
